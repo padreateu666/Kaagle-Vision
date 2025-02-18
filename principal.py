d19f2ebf87f2d1cef7d73e04e86e1e4a93d9c59c
padreateu666 <luisesborges@gmail.com>
2025-02-18 19:10:11 -0300

    Criacao do repo

---
+++ principal.py
@@ -0,0 +1,9 @@
+import os, warnings
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+import numpy as np 
+import tensorflow as tf
+from twnsoflow.keras.preprocessing import image_dataset_from_directory
+
+
